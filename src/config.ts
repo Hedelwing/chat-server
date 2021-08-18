8a9be0f644559b85d34d33adfefcd1a5126d92e9
Хедельвинг <rosiestrike@bk.ru>
2021-08-18 20:07:23 +0300

    fix endpoints

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const {
     DB_PASS = "dafno4ka",
     DB_HOST = "cluster-mzc7p.azure.mongodb.net",
     DB_NAME = "chat",
-    FRONT_URI = process.env.NODE_ENV === "production" ? "https://hedelwing-chat.herokuapp.com/" : "http://localhost:8080"
+    FRONT_URI = process.env.NODE_ENV === "production" ? "https://hedelwing-chat.herokuapp.com" : "http://localhost:8080"
 } = process.env
 
 export const rootDir = __dirname
